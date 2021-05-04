28cdb370ae1201e4988d6db1eef909e3f6e9d50b
breenvinicius@gmail.com <breno-vinicius>
2021-05-04 17:20:46 -0300

    docker compose, webapp, nginx, mysql

---
+++ entrypointdb/database.sql
@@ -0,0 +1,22 @@
+SET character_set_client = utf8;
+SET character_set_connection = utf8;
+SET character_set_results = utf8;
+SET collation_connection = utf8_general_ci;
+
+CREATE DATABASE desafio_um_pfa
+  CHARACTER SET utf8
+  COLLATE utf8_general_ci;
+
+USE desafio_um_pfa;
+
+CREATE TABLE desafio_um_pfa.modulo (
+	id BIGINT UNSIGNED auto_increment NOT NULL,
+	nome varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	CONSTRAINT modulo_PK PRIMARY KEY (id),
+	CONSTRAINT modulo_nome_UN UNIQUE KEY (nome)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci;
+
+INSERT INTO desafio_um_pfa.modulo (`nome`) VALUES ("Docker"), ("Integração contínua"), ("Kubernetes"), ("Fundamentos de arquitetura de software"), ("Comunicação");
